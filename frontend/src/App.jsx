714fc3422827e6ec4c5fbbbf067181f93b49ef84
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2024-03-19 22:04:49 +0530

    create basic structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <>
-      <h1>shiva</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
